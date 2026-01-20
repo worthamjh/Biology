774146294e8485e6b1c1b64a62c02b69850b0863
worthamjh <worthamjh@gmail.com>
2026-01-20 10:57:13 -0600

    Move script tag and add password validation logic
    Relocated the Sandbox.js script tag to the end of the body in Sandbox.html for better loading performance. Replaced the console log in Sandbox.js with a password prompt and validation logic, and added several commented code snippets for future reference.

--- Sandbox.js
+++ Sandbox.js
@@ -1,1 +1,56 @@
-console.log('Boners')
+// let random = Math.random();
+// if(random < 0.5){
+//     console.log('Less than 0.5')
+// } else{
+//     console.log('number is atleast 0.5')
+// }
+// console.log(random);
+
+// let dayOfWeek= prompt('What day is it?').toLowerCase();
+
+// if (dayOfWeek === 'monday'){
+//     console.log('BOOOOO')
+// } else if (dayOfWeek=== 'saturday'){
+//     console.log('YASSSSS')
+// } else if (dayOfWeek=== 'fridaFRIDayy'){
+//     console.log('Weiner')
+// } else if(dayOfWeek=== 'wednesday'){
+//     console.log('Hump DaYYYYYY')
+// } else{
+//     console.log('The fuck are you talking about')
+// }
+
+// let age = 110;
+
+// if(age<5){
+//     console.log('That will be $0, please.')
+// } else if (age<11){
+//     console.log('That will be $10, please.')
+// } else if (age<65){
+//     console.log('That will be $20, please.')
+// } else if (age<111){
+//     console.log('That will be $10 old timer.')
+// } else if (age>=111){
+//     console.log('How are you alive')
+// }
+// let phrase = 'go'
+// if (phrase==='stop'){
+//     console.log('red')
+// } else if (phrase==='slow'){
+//     console.log('yellow')
+// } else if (phrase==='go'){
+//     console.log('green')
+// } else {
+//     console.log('purple')
+// }
+
+let password = prompt('Please enter new password')
+
+if (password.length >= 6)
+    if (password.indexOf(' ')===-1){
+    console.log('okay')
+    } else {
+    console.log('that password sucks ass')
+} else{
+    console.log('you suck ass')
+}
