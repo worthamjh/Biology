5f1a8064bbcc1e850cbcae8ee121744dcca7f36b
worthamjh <worthamjh@gmail.com>
2026-01-22 14:20:37 -0600

    Update loop to count down from 25 by 5
    Replaces the previous loop with a new one that logs numbers from 25 down to 0 in steps of 5. Also adds commented examples of other loop variations for reference.

--- Sandbox.js
+++ Sandbox.js
@@ -184,6 +184,14 @@
 //     console.log('Da ba dee da ba daa')
 // }
 
-for (let i=0; i<=20; i+=2){
+// for (let i=1; i<=20; i+=2){
+//     console.log(i)
+// }
+
+// for(let i=100; i>=0; i-=10){
+//     console.log(i)
+// }
+
+for (let i=25; i>=0; i-=5){
     console.log(i)
 }
