33c824292b72ffc5a670b74db0c0bc5657163818
worthamjh <worthamjh@gmail.com>
2026-01-26 18:21:36 -0600

    Add isSnakeEyes function to check for snake eyes
    Introduced the isSnakeEyes function, which logs 'Snake Eyes!' if both dice are 1, otherwise logs 'Not Snake Eyes!'.

--- Sandbox.js
+++ Sandbox.js
@@ -398,3 +398,11 @@
 //     }
 //     console.log(result)
 // }
+
+function isSnakeEyes(die1, die2){
+    if(die1 === 1 && die2 === 1){
+        console.log('Snake Eyes!');
+    }
+    else{console.log('Not Snake Eyes!');
+    }
+}
