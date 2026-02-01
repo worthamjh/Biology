041c43b21785a62b3f9b39f7071e4f10658f8114
worthamjh <worthamjh@gmail.com>
2026-02-01 13:54:21 -0600

    Comment out slides iframe; update script
    Disable the embedded Google Slides iframe in B-1-1.html by wrapping it in an HTML comment. In script.js remove the large footer data structure and many debug loops, leaving a small commented loop and a new query for inputs (const checkBox = document.querySelectorAll('input')). These changes simplify the page and script for further development or debugging.

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-const footer=[
-    {title:'Canvas',
-     image:'https://raw.githubusercontent.com/worthamjh/Biology/refs/heads/main/Logos/I.CanvasLogo.png',
-     link:'https://wgsd.instructure.com/login/canvas'},
-    {title:'Clever',
-     image:'https://raw.githubusercontent.com/worthamjh/Biology/refs/heads/main/Logos/I.Clever.Logo.png',
-     link:'https://clever.com/oauth/district-picker?channel=clever&client_id=4c63c1cf623dce82caac&confirmed=true&redirect_uri=https%3A%2F%2Fclever.com%2Fin%2Fauth_callback&response_type=code&state=073ccec89c0496afb0b1918689a1cdf1380ad0fa00287446edd93fde044263c7'},
-    {title:'Gmail',
-     image:'https://raw.githubusercontent.com/worthamjh/Biology/refs/heads/main/Logos/I.GmailLogo.png',
-     link:'https://workspace.google.com/intl/en-US/gmail/'},
-    {title:'Google Drive',
-     image:'https://raw.githubusercontent.com/worthamjh/Biology/refs/heads/main/Logos/I.GoogleDriveLogo.png',
-     link:'https://workspace.google.com/products/drive/'},
-    {title:'Webster Groves School District',
-     image:'https://raw.githubusercontent.com/worthamjh/Biology/refs/heads/main/Logos/I.WGLogo.2.png',
-     link:'https://www.webster.k12.mo.us/'},
-    {title:'YouTube',
-     image:'https://raw.githubusercontent.com/worthamjh/Biology/refs/heads/main/Logos/I.YoutubeLogo.png',
-     link:'https://www.youtube.com'},
-    {title:'Special School Distrrict',
-     image:'https://raw.githubusercontent.com/worthamjh/Biology/refs/heads/main/Logos/I.SSDLogo.png',
-     link:'https://www.ssdmo.org'}     
-];
-
-// for (let i=0; i <footer.length; i++){
-//     console.log(i,footer[i])
+// const links = document.querySelectorAll('li');
+// for (let link of links) {
+//     console.log(link.)
 // }
 
-// for (let i=0; i < footer.length; i++ ){
-//     const ass= footer[i];
-//     console.log(`yello ${i}`)
-//     for (let j=0; j < ass.length; j++){
-//         console.log(j,ass[j])
-//     }
-// }  
-//     for(let j=0; j < ass.length; j++){
-//         console.log(ass[j])
-//     }
-// }
+const checkBox = document.querySelectorAll('input')
